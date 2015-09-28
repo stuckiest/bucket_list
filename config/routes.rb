99f15e62c3e7b8582c6cfc2e4f4487fb476d678c
bondparkerbond <parkerbond@gmail.com>
2015-09-28 17:10:04 -0600

    Controllers, routes, require authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+
   devise_for :users
+
+  get 'users/:id', to: 'users#profile', as: :profile
+
+  resources :users do
+    resources :goals
+  end
+
+
+  root to: 'users#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
